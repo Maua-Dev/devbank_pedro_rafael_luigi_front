6ff1be6f03fe07cc7f1b304b1b5eedd4ecf717e9
Pedro Juan <pedrojuancoelho@gmail.com>
2026-04-26 17:41:29 -0300

    Segundo Commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import TelaInicial from './pages/Tela inicial'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TelaInicial />
   </React.StrictMode>,
 )
